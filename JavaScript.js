0ed6c765be9499464d1fbd4b08ca6f3a0674ff07
Kiril0326 <talmackiril@gmail.com>
2022-11-02 20:30:12 +0200

    first commit

---
+++ JavaScript.js
@@ -0,0 +1,85 @@
+// const arr = [];
+// const arr1 = [1, 2, 3, 4,"salut", "tri", true];
+// console.log(arr);
+// console.log(arr1.length);
+
+// const user = prompt("Cum te numesti")
+// const user1 = prompt("Citi ani ai")
+
+// const userArray = []
+// const userArray1 = ["Ion"]
+
+// userArray.push(userArray1)
+// userArray.push(user)
+// userArray.push(user1)
+// console.log(userArray);
+
+// let obj = {
+//     name:"Vasile",
+//     age:"26",
+//     gen:"M"
+// }
+
+// console.log(obj);
+// console.log(obj.name, obj.age);
+
+// let ojb_1 = {
+//     name:"Nina",
+//     age:"39",
+//     gen:"F",
+//     nameChild: {
+//         name:"Viorel",
+//         age:"5",
+//         gen:"M"
+//     }
+// }
+
+// console.log(typeof ojb_1.nameChild.age);
+
+
+// const user = [
+//     {name:"Alex"},
+//     {name:"Ion"}
+// ]
+//     user.forEach(el => {
+//         console.log(el);
+//     });
+
+// const array = [
+//     "1",
+//     "2",
+//     "3",
+//     "4",
+//     "5",
+//     "6"
+// ];
+// array.forEach(el => {
+//     // console.log(el.includes("6"));
+
+//     if(el.includes("cinci")){
+//         console.log(el);
+//     }else{
+//         console.log("netu");
+//     }
+// })
+
+const array = [
+    "1",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6"
+];
+
+function name(){
+    console.log(array);
+}
+
+name()
+
+function name1(params){
+    console.log(params);
+}
+
+name1("salu")
